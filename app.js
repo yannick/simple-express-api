ec2eeb21153098a165eea7d86e4fcd36787f2e42
Greg <georgi@fonii.com>
2013-09-11 18:56:13 +0200

    used mongo queries and simple charts, not cleaned

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var express = require('express');
- items = require('./items');
+var fs        = require('fs');
+
+items = require('./items');
 var app = express();
 
-app.get('/items/:id', items.findById);
+var publicdir = __dirname;
+app.use(function(req, res, next) {
+  if (req.path.indexOf('.') === -1) {
+    var file = publicdir + req.path + '.html';
+    fs.exists(file, function(exists) {
+      if (exists)
+        req.url += '.html';
+      next();
+    });
+  }
+  else
+    next();
+});
+app.use(express.static(publicdir));
 
+app.get('/items/:id', items.findById);
+app.get('/items', items.findAll);
 app.listen(process.env.PORT || 4730);
--- items.js
+++ items.js
@@ -9,8 +9,8 @@ db = new Db('ricardo-crawler-2', server);
  
 db.open(function(err, db) {
     if(!err) {
-        console.log("Connected to 'items' database");
-        db.collection('items', {strict:true}, function(err, collection) {
+        console.log("Connected to 'ricardo-crawler-2' database");
+        db.collection('results_per_category', {strict:true}, function(err, collection) {
             if (err) {
                 console.log("The 'items' collection doesn't exist. Creating it with sample data...");
             }
@@ -21,9 +21,17 @@ db.open(function(err, db) {
 exports.findById = function(req, res) {
     var id = req.params.id;
     console.log('Retrieving item: ' + id);
-    db.collection('items', function(err, collection) {
+    db.collection('results_per_category', function(err, collection) {
         collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
             res.send(item);
+        });
+    });
+};
+
+exports.findAll = function(req, res) {
+    db.collection('results_per_category', function(err, collection) {
+        collection.find().toArray(function(err, results) {
+            res.send(results);
         });
     });
 };
